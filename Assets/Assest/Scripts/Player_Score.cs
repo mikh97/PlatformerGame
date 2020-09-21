e860e11d12f0ec29fe30ce768b474a3b38792455
Mikhail Sumawan <mikh1738@gmail.com>
2020-09-21 14:24:41 +0700

    Project commit

---
+++ Assets/Assest/Scripts/Player_Score.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player_Score : MonoBehaviour
+{
+    private float timeLeft = 200;
+    public int playerScore = 0;
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
+
+
+    void Start()
+    {
+        //Just for testing
+        DataManagement.datamanagement.LoadData();
+    }
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int) timeLeft);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore);
+        if (timeLeft < 0.1f)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig){
+        if (trig.gameObject.name == "victory flag")
+        {
+            CountScore();
+        }
+
+        if (trig.gameObject.name == "coin")
+        {
+            playerScore += 100;
+            Destroy(trig.gameObject);
+        }
+
+
+    }
+
+    void CountScore() {
+        playerScore = playerScore + (int)(timeLeft * 10);
+        DataManagement.datamanagement.highScore = playerScore + (int)(timeLeft * 10);
+        DataManagement.datamanagement.SaveData();
+
+    }
+}
---
+++ Assets/SoundManagerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+
+    public static AudioClip walkingSound, jumpSound, deathSound;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkingSound = Resources.Load<AudioClip>("walking");
+        jumpSound = Resources.Load<AudioClip>("jump");
+        deathSound = Resources.Load<AudioClip>("death");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "walking":
+                audioSrc.PlayOneShot(walkingSound);
+                break;
+            case "jump":
+                audioSrc.PlayOneShot(jumpSound);
+                break;
+            case "death":
+                audioSrc.PlayOneShot(deathSound);
+                break;
+        }
+    }
+}
